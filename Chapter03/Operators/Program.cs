6be1e59eb351caff505fb8056d792d34b6816cd7
jsnow-pc <jsnow-pc@jsnow-pcs-iMac.local>
2021-01-28 21:03:45 +0800

    day1,day2,day3

---
+++ Chapter03/Operators/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 3;
+            int b = a++;
+            System.Console.WriteLine($"a is {a},b is {b}");
+        }
+    }
+}
